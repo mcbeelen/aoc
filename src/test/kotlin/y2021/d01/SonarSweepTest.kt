2d4d74f71be0d0ed627e1f0168066ae5fb1f7734
Marco Beelen <mbeelen@payplaza.com>
2021-12-01 09:55:40 +0100

    Solved Y2021 Day 1: Sonar Sweep

---
+++ src/test/kotlin/y2021/d01/SonarSweepTest.kt
@@ -0,0 +1,30 @@
+package y2021.d01
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+
+class SonarSweepTest {
+
+    @Test
+    fun examplePartOne() {
+        assertThat(solveSonarSweep(testInput), equalTo(7))
+    }
+
+    @Test
+    fun examplePartTwo() {
+        assertThat(solveSlidingSonarSweep(testInput), equalTo(5))
+    }
+}
+
+
+const val testInput = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263"""
