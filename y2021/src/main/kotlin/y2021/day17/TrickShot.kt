6d94ebcdcaac691d141583911a0dddad2d4b731c
Marco Beelen <marco@mcbeelen.net>
2021-12-18 11:59:57 +0100

    Solved y2021d17 with math and bit of brute force

--- y2021/src/main/kotlin/y2021/day17/TrickShot.kt
+++ y2021/src/main/kotlin/y2021/day17/TrickShot.kt
@@ -1,17 +1,70 @@
 package y2021.day17
 
 import util.puzzle.AdventOfCodePuzzle
+import java.lang.Math.abs
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 class TrickShot(testInput: String = "") : AdventOfCodePuzzle(testInput) {
+    // Example: target area: x=20..30, y=-10..-5
+    // Actual:  target area: x=32..65, y=-225..-177
+    val minX = input[0].substringAfter("x=").substringBefore("..").toInt()
+    val maxX = input[0].substringAfter("..").substringBefore(",").toInt()
+
+    val maxY = input[0].substringAfter("y=").substringAfter("..").trim().toInt()
+    val minY = input[0].substringAfter("y=").substringBefore("..").toInt()
 
     override fun solvePartOne(): Int {
-        println(input[0])
-        TODO("Solve me")
+        return (abs(minY) * (abs(minY) - 1)) / 2
     }
 
     override fun solvePartTwo(): Int {
-        TODO("Solve me")
+
+        val minVx = calculateMinimumVelocityToReach(minX) - 1
+        val maxVx = maxX
+        val minVy = minY
+        val maxVy = abs(minY)
+
+        val candidates = (minVx .. maxVx).flatMap { x ->
+            (minVy..maxVy).map { y -> Pair(x, y ) }
+        }
+
+        return candidates.count { hitsTargetArea(it) }
     }
+
+    fun hitsTargetArea(candidate: Pair<Int, Int>) : Boolean {
+        val (cx, cy) = candidate
+        val minT = 1
+        val maxT = 2 * (abs(minY)) + 1
+        return (minT .. maxT)
+            .map { Triple(cx, cy, it) }
+            .any { hitsTargetArea(it) }
+    }
+
+    fun hitsTargetArea(candidate: Triple<Int, Int, Int>) : Boolean {
+        val (cx, cy, ct) = candidate
+        val xt = xPosAfterT(cx, ct)
+        val yt = yPosAfterT(cy, ct)
+        return xt in minX .. maxX &&
+                yt in minY .. maxY
+    }
+}
+
+internal fun xPosAfterT(cx: Int, ct: Int) = if(ct >= cx) {
+    ((cx + 1 ) * cx) / 2
+} else {
+    (cx * ct) - ((ct - 1) * ct) / 2
+}
+
+internal fun yPosAfterT(cy: Int, ct: Int) = (ct  * cy) - ((ct - 1) * ct) / 2
+
+fun calculateMinimumVelocityToReach(distance: Int): Int {
+    val candidate = floor(sqrt((distance * 2).toDouble())).toInt()
+    val reachedDistance = ((candidate * (candidate + 1)) / 2)
+    if (reachedDistance == distance) {
+        return candidate
+    }
+    return candidate + 1
 }
 
 fun main() {
--- y2021/src/test/kotlin/y2021/day17/TrickShotTest.kt
+++ y2021/src/test/kotlin/y2021/day17/TrickShotTest.kt
@@ -2,6 +2,7 @@ package y2021.day17
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.junit.Assert.assertTrue
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.Int.Companion.MIN_VALUE
@@ -9,32 +10,76 @@ import kotlin.Int.Companion.MIN_VALUE
 class TrickShotTest {
 
     @Test
-    fun examplePartOne() {
-        val puzzle = TrickShot(testInput)
-        assertThat(puzzle.solvePartOne(), equalTo(0))
+    fun calculateMinimumVelocityToReach() {
+        assertThat(calculateMinimumVelocityToReach(10), equalTo(4))
+        assertThat(calculateMinimumVelocityToReach(11), equalTo(5))
     }
 
     @Test
-    @Ignore
+    fun examplePartOne() {
+        val puzzle = TrickShot(testInput)
+        assertThat(puzzle.solvePartOne(), equalTo(45))
+    }
+
+    @Test
     fun actualPartOne() {
         val puzzle = TrickShot()
-        assertThat(puzzle.solvePartOne(), equalTo(MIN_VALUE))
+        assertThat(puzzle.solvePartOne(), equalTo(25200))
+    }
+
+    @Test
+    fun xPosAfterT() {
+        assertThat(xPosAfterT(7, 1), equalTo(7))
+        assertThat(xPosAfterT(7, 2), equalTo(13))
+        assertThat(xPosAfterT(7, 3), equalTo(18))
+        assertThat(xPosAfterT(7, 4), equalTo(22))
+        assertThat(xPosAfterT(7, 5), equalTo(25))
+        assertThat(xPosAfterT(7, 6), equalTo(27))
+        assertThat(xPosAfterT(7, 7), equalTo(28))
+        assertThat(xPosAfterT(7, 8), equalTo(28))
+        assertThat(xPosAfterT(7, 9), equalTo(28))
+    }
+
+    @Test
+    fun yPosAfterT() {
+        assertThat(yPosAfterT(10, 1), equalTo(10))
+        assertThat(yPosAfterT(10, 2), equalTo(19))
+        assertThat(yPosAfterT(10, 3), equalTo(27))
+        assertThat(yPosAfterT(10, 4), equalTo(34))
+        assertThat(yPosAfterT(10, 5), equalTo(40))
+        assertThat(yPosAfterT(10, 6), equalTo(45))
+        assertThat(yPosAfterT(10, 7), equalTo(49))
+        assertThat(yPosAfterT(10, 8), equalTo(52))
+        assertThat(yPosAfterT(10, 9), equalTo(54))
+        assertThat(yPosAfterT(10, 10), equalTo(55))
+        assertThat(yPosAfterT(10, 11), equalTo(55))
+        assertThat(yPosAfterT(10, 20), equalTo(10))
+        assertThat(yPosAfterT(10, 21), equalTo(0))
+
+    }
+
+    @Test
+    fun itHitsTarget() {
+        val puzzle = TrickShot(testInput)
+        assertTrue(puzzle.hitsTargetArea(Pair(6, 2)))
+        assertTrue(puzzle.hitsTargetArea(Pair(6, 9)))
+        assertTrue(puzzle.hitsTargetArea(Pair(23, -10)))
+        assertTrue(puzzle.hitsTargetArea(Pair(27, -5)))
     }
 
     @Test
     fun examplePartTwo() {
         val puzzle = TrickShot(testInput)
-        assertThat(puzzle.solvePartTwo(), equalTo(0))
+        assertThat(puzzle.solvePartTwo(), equalTo(112))
     }
 
     @Test
-    @Ignore
     fun actualPartTwo() {
         val puzzle = TrickShot()
-        assertThat(puzzle.solvePartTwo(), equalTo(MIN_VALUE))
+        assertThat(puzzle.solvePartTwo(), equalTo(3012))
     }
 
 }
 
 
-private const val testInput = """PASTE_HERE"""
+private const val testInput = """target area: x=20..30, y=-10..-5"""
