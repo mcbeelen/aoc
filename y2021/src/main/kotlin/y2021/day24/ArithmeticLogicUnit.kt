15d9cefe1f2e6f9620fb434e6f40d85c87bbe4b6
Marco Beelen <marco@mcbeelen.net>
2021-12-24 08:02:36 +0100

    Adds structure for Arithmetic Logic Unit y2021d24

---
+++ y2021/src/main/kotlin/y2021/day24/ArithmeticLogicUnit.kt
@@ -0,0 +1,19 @@
+package y2021.day24
+
+import util.puzzle.AdventOfCodePuzzle
+
+class ArithmeticLogicUnit(testInput: String = "") : AdventOfCodePuzzle(testInput) {
+
+    override fun solvePartOne(): Int {
+        println(input[0])
+        TODO("Solve me")
+    }
+
+    override fun solvePartTwo(): Int {
+        TODO("Solve me")
+    }
+}
+
+fun main() {
+    ArithmeticLogicUnit().getAnswers()
+}
