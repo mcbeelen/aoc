6f8d8e652aa84ad85d9db55ad3058d9138168c88
Marco Beelen <mcbeelen@gmail.com>
2024-12-03 06:39:12 +0100

    Input for y2024 day 03

---
+++ y2024/src/main/kotlin/y2024/day03/MullItOver.kt
@@ -0,0 +1,19 @@
+package y2024.day03
+
+import util.puzzle.AdventOfCodePuzzle
+
+class MullItOver(testInput: String = "") : AdventOfCodePuzzle(testInput) {
+
+    override fun solvePartOne(): Int {
+        println(input[0])
+        TODO("Solve me")
+    }
+
+    override fun solvePartTwo(): Int {
+        TODO("Solve me")
+    }
+}
+
+fun main() {
+    MullItOver().getAnswers()
+}
