64d55906dd0e1a79733a9ba2081e2a1c5db91e0b
Marco Beelen <mcbeelen@gmail.com>
2024-12-03 06:42:39 +0100

    Copied testInput from the puzzle description

--- y2024/src/test/kotlin/y2024/day03/MullItOverTest.kt
+++ y2024/src/test/kotlin/y2024/day03/MullItOverTest.kt
@@ -37,4 +37,4 @@ class MullItOverTest {
 }
 
 
-private const val testInput = """PASTE_HERE"""
+private const val testInput = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
