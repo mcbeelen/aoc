4f5b1b65775fe9f3e7bf48c64dbfaeb5cf130b0e
Marco Beelen <mcbeelen@gmail.com>
2024-12-05 06:43:55 +0100

    Start for y2024 day 05

---
+++ y2024/src/main/kotlin/y2024/day05/PrintQueue.kt
@@ -0,0 +1,19 @@
+package y2024.day05
+
+import util.puzzle.AdventOfCodePuzzle
+
+class PrintQueue(testInput: String = "") : AdventOfCodePuzzle(testInput) {
+
+    override fun solvePartOne(): Int {
+        println(input[0])
+        TODO("Solve me")
+    }
+
+    override fun solvePartTwo(): Int {
+        TODO("Solve me")
+    }
+}
+
+fun main() {
+    PrintQueue().getAnswers()
+}
