48ab64b53c1adef968c092e218a9b62af3168a5b
Marco Beelen <mcbeelen@gmail.com>
2024-12-06 07:40:38 +0100

    Fetched challenge for 20241206

---
+++ y2024/src/main/kotlin/y2024/day06/GuardGallivant.kt
@@ -0,0 +1,19 @@
+package y2024.day06
+
+import util.puzzle.AdventOfCodePuzzle
+
+class GuardGallivant(testInput: String = "") : AdventOfCodePuzzle(testInput) {
+
+    override fun solvePartOne(): Int {
+        println(input[0])
+        TODO("Solve me")
+    }
+
+    override fun solvePartTwo(): Int {
+        TODO("Solve me")
+    }
+}
+
+fun main() {
+    GuardGallivant().getAnswers()
+}
